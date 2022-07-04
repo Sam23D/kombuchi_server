d7f09a8997b01be1fcbb95b1e42b0e4040eb0046
sam23d <dev.sam23d@gmail.com>
2022-07-04 16:12:13 -0600

    Adds user settings page

--- lib/kombuchi_server_web/router.ex
+++ lib/kombuchi_server_web/router.ex
@@ -102,7 +102,6 @@ defmodule KombuchiServerWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/profile", UserSettingsController, :profile
-    get "/users/dashboard", UserSettingsController, :dashboard
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
   end
 
